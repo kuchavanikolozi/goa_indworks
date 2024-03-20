d565ac19fb461eaf0e94f5b27b27b7bf0524017f
kuchavanikolozi <booking.rzakabot@gmail.com>
2024-03-20 19:58:36 +0400

    commit

---
+++ project 0/my family .py
@@ -0,0 +1,4 @@
+#i want talk about my family.
+family = "Mom, Dad, Two older brothers, two nephews, and foushee."
+
+print("hello world!,i want talk about my family,They are members of my family"  + " " + family + str(" my family is very funny"))
---
+++ project 1/my autobiography.py
@@ -0,0 +1,8 @@
+#i want talk about myself.
+name = "Nikolozi"
+surname = "Kuchava"
+age = 15 
+loves_reading = True
+loves_writing = True
+
+print(" Hello World!, i want talk about me!, my name is" + " " + name + " " + surname + (" and i am ") + str (age) + " Years Old" + str(" my hobby is writing, i've had wrote four stories.")
