b468464d2919e04758c3318dde8dcd033ebc9909
Nikolozi <158770258+kuchavanikolozi@users.noreply.github.com>
2024-06-21 19:56:07 +0400

    Update my autobiography.py

--- project 1/my autobiography.py
+++ project 1/my autobiography.py
@@ -2,7 +2,5 @@
 name = "Nikolozi"
 surname = "Kuchava"
 age = 15 
-loves_reading = True
-loves_writing = True
 
 print(" Hello World!, i want talk about me!, my name is" + " " + name + " " + surname + (" and i am ") + str (age) + " Years Old" + str(" my hobby is writing, i've had wrote four stories.")
